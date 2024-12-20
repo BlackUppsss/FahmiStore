421c51bbc77110322b7a54011361d8058cbe662c
neverendingloneliness <sahmuraivam43@gmail.com>
2024-12-20 17:01:00 +0700

    update structure

--- Node/HistoryNode.java
+++ Node/HistoryNode.java
@@ -1,32 +1,6 @@
 package Node;
 
-class History{
-    private int id;
-    private TransactionNode transaction;
-
-    public History(int id){
-        this.id = id;
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public TransactionNode getTransaction(){
-        return transaction;
-    }
-
-    public void setTransaction(TransactionNode transaction){
-        this.transaction = transaction;
-    }
-
-    public String info(){
-        String transactionInfo = (transaction != null) ? transaction.info() : "transaction: belum ada";
-        return "History ID: " + id + " |  " + transactionInfo;
-        
-    }
-
-}
+import Object.History;
 
 public class HistoryNode {
     History data;
--- Node/TransactionNode.java
+++ Node/TransactionNode.java
@@ -1,61 +1,7 @@
 package Node;
 
-import java.sql.Timestamp;
+import Object.Transaction;
 
-class Transaction{
-    private int id;
-    private UserNode user;
-    private GadgetNode gadget;
-    private Timestamp tanggalTransaksi;
-    
-    public enum Status{
-        diterima, ditolak, diproses
-    }
-
-    private Status status;
-
-    public Transaction(int id, Timestamp tanggalTransaksi, Status status){
-        this.id = id;
-        this.tanggalTransaksi = tanggalTransaksi;
-        this.status = status;
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public Timestamp tangTimestamp(){
-        return tanggalTransaksi;
-    }
-
-    public UserNode getUser(){
-        return user;
-    }
-
-    public Status getStatus(){
-        return status;
-    }
-
-    public void setUser(UserNode user){
-        this.user = user;
-    }
-
-    public GadgetNode getGadget(){
-        return gadget;
-    }
-
-    public void setGadget(GadgetNode gadget){
-        this.gadget = gadget;
-    }
-
-    public String info(){
-        String userInfo = (user != null) ? user.info() : "user: belum ada";
-        String gadgetInfo = (gadget != null) ? gadget.info() : "gadget: belum ada";
-        return "Transaction ID: " + id + " | Status: " + status + " | Date: " + tanggalTransaksi + " | " + userInfo + " | " + gadgetInfo;
-    }
-
-
-}
 
 public class TransactionNode {
     Transaction data;
